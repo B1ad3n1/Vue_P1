a55a32b64ae7d4e46d0c19510108902cfbb5d514
B1ad3n1 <KotovRR22@st.ithub.ru>
2025-01-15 19:38:35 +0300

    v0.2

---
+++ src/App.vue
@@ -0,0 +1,74 @@
+<template>
+  <header>
+    <div class="aheader">
+      <div>
+        <a class="logo">Logo</a>
+      </div>
+      <nav class="nav">
+        <a class="nava">About</a>
+        <a class="nava">Work</a>
+        <a class="nava">Contact</a>
+      </nav>
+    </div>
+    <div class="bheader">
+      <div class="ab">
+        <h1>Heh</h1>
+      </div>
+      <div>
+
+      </div>
+    </div>
+    <div>
+
+    </div>
+  </header>
+  <div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+  </div>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style scoped>
+.aheader{
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: 24px;
+  width: 1280px;
+  height: 78px;
+}
+
+.logo{
+  padding: 5px;
+  font-weight: bold;
+}
+.nav{
+  display: flex;
+
+}
+.nava{
+  padding: 5px;
+}
+
+.bheader{
+  width: 1280px;
+  height: 440px;
+  gap: 68px;
+}
+.ab{
+  width: 620px;
+  height: 328px;
+  gap: 48px;
+}
+
+</style>
